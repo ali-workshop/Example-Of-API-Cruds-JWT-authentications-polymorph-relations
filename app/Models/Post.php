56e51dd410d0ece2bbefc2aa2d3f3ebd785d08f1
ALI ALI <alirafikali22@gmail.com>
2024-05-20 08:44:59 +0300

    build the realtions one to many betweent he post and category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -12,4 +13,13 @@ class Post extends Model
                         'title',
                         'body',
                          ];
+
+
+
+
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
