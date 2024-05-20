fe6a42101c115e55c234f20bea7133da2e953773
ALI ALI <alirafikali22@gmail.com>
2024-05-20 10:43:33 +0300

    add morph relations

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
